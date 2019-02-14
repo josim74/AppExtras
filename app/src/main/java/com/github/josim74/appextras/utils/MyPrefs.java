eec2318e5ca41610cfa384d75df04067f55318b8
Josim Uddin <mdjosimuddin712@gmail.com>
2019-02-14 15:04:11 +0600

    Initial commit

---
+++ app/src/main/java/com/github/josim74/appextras/utils/MyPrefs.java
@@ -0,0 +1,18 @@
+package com.github.josim74.appextras.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class MyPrefs {
+
+    /*public static Builder with(Context context) {
+
+
+        return new Builder();
+    }
+
+ public class Builder {
+        SharedPreferences preferences;
+        SharedPreferences.Editor editor;
+    }*/
+}
